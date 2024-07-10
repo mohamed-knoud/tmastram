be45e1cb0286dc2a5d6cb0a838e3ed40fc1f0e75
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2024-07-11 00:21:15 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -480,7 +480,7 @@ const handleRight4=()=>{
             </div>
         </div>
 
-      <div style={{width:'100%',backgroundColor:'#2f2f2F',textAlign:'center',color:'#a4b2c8'}}>
+      <div style={{padding:'20px',width:'100%',backgroundColor:'#2f2f2F',textAlign:'center',color:'#a4b2c8'}}>
      <Link to=""> تنبيه جميع الافلام والمسلسلات المتوفرة على منصتنا محملة من مواقع الانترنت لمشاهدة الافلام والمسلسلات بشكل افضل على المنصات الرسمية </link>اضغط هنا
 تنبيه ادارة المنصة غير مسؤولة عن محتوى الافلام
 والمسلسلات
